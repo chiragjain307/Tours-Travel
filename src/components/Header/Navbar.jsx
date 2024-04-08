3a648dc9f56cee303e89da7a154e4e09786f6d3a
chiragjain307 <chiragjain.jain7@gmail.com>
2024-04-08 22:24:29 +0530

    TopPackages

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { HiMenuAlt3 } from "react-icons/hi"
-import { MdClose } from "react-icons/md";
+import { HiMenuAlt3 } from 'react-icons/hi'
+import { MdClose } from 'react-icons/md';
 
 function Navbar() {
     const navItems = ['Home', 'Tickets', 'Explore', 'Activity']
@@ -12,38 +12,38 @@ function Navbar() {
     }
 
     return (
-        <nav className="w-full h-24 flex justify-center items-center sticky top-0 z-50 bg-white">
-            <div className="container mx-auto lg:px-3 w-full">
-                <div className=" flex gap-x-4 lg:w-full w-11/12 mx-auto h-full justify-between items-center">
-                    <div className="flex items-center gap-x-2">
-                        <Link to='/' className="text-primary font-bold text-lg">Tripco</Link>
+        <nav className='w-full h-24 flex justify-center items-center sticky top-0 z-50 bg-white'>
+            <div className='container mx-auto lg:px-3 w-full'>
+                <div className=' flex gap-x-4 lg:w-full w-11/12 mx-auto h-full justify-between items-center'>
+                    <div className='flex items-center gap-x-2'>
+                        <Link to='/' className='text-primary font-bold text-lg'>Tripco</Link>
                     </div>
 
                     <div>
-                        <ul className="flex items-center xl:gap-12 gap-x-4 max-lg:hidden">
+                        <ul className='flex items-center xl:gap-12 gap-x-4 max-lg:hidden'>
                             {navItems.map((item, index) => {
                                 if (item === 'Home') {
                                     return (
-                                        <Link to='/' key={index} className="leading-normal no-underline text-black text-lg hover:text-black">{item}</Link>
+                                        <Link to='/' key={index} className='leading-normal no-underline text-black text-lg hover:text-black'>{item}</Link>
                                     )
                                 }
                                 else {
                                     return (
-                                        <Link to={item.toLowerCase()} key={index} className="leading-normal no-underline text-light text-lg hover:text-black">{item}</Link>
+                                        <Link to={item.toLowerCase()} key={index} className='leading-normal no-underline text-light text-lg hover:text-black'>{item}</Link>
                                     )
                                 }
                             })}
                         </ul>
                     </div>
 
-                    <div className="flex gap-4 max-lg:hidden">
-                        <button className="bg-transparent rounded shadow h-12 px-6 outline-none text-light hover:bg-primary hover:text-white cursor-pointer text-base transition-bg hover:border hover:border-primary" >Sign In</button>
-                        <button className="bg-primary rounded shadow h-12 px-6 outline-none text-white hover:bg-white hover:text-primary cursor-pointer text-base transition-bg hover:border hover:border-primary">Sign Up</button>
+                    <div className='flex gap-4 max-lg:hidden'>
+                        <button className='bg-transparent rounded shadow h-12 px-6 outline-none text-light hover:bg-primary hover:text-white cursor-pointer text-base transition-bg hover:border hover:border-primary' >Sign In</button>
+                        <button className='bg-primary rounded shadow h-12 px-6 outline-none text-white hover:bg-white hover:text-primary cursor-pointer text-base transition-bg hover:border hover:border-primary'>Sign Up</button>
                     </div>
 
                     <button
                         onClick={showDropdown}
-                        className="lg:hidden text-[22px] cursor-pointer text-black"
+                        className='lg:hidden text-[22px] cursor-pointer text-black'
                     >
                         {dropdown ? <MdClose /> : <HiMenuAlt3 />}
                     </button>
@@ -51,16 +51,16 @@ function Navbar() {
                     <div className='md:hidden'>
                         {dropdown && (
                             <div className='lg:hidden fixed top-24 w-full left-0 right-0 bg-primary transition-all items-baseline gap-4 '>
-                                <ul className="flex flex-col justify-center w-full">
+                                <ul className='flex flex-col justify-center w-full'>
                                     {navItems.map((item, index) => {
                                         if (item === 'Home') {
                                             return (
-                                                <Link to='/' key={index} className="px-6 h-10 flex items-center leading-normal no-underline text-white font-bold text-lg hover:text-primary text-[15px] border-0 border-b border-[#ffffff1a] border-solid">{item}</Link>
+                                                <Link to='/' key={index} className='px-6 h-10 flex items-center leading-normal no-underline text-white font-bold text-lg hover:text-primary text-[15px] border-0 border-b border-[#ffffff1a] border-solid'>{item}</Link>
                                             )
                                         }
                                         else {
                                             return (
-                                                <Link to={item.toLowerCase()} key={index} className="px-6 h-10 flex items-center leading-normal no-underline text-white font-bold text-lg hover:text-primary text-[15px] border-0 border-b border-[#ffffff1a] border-solid">{item}</Link>
+                                                <Link to={item.toLowerCase()} key={index} className='px-6 h-10 flex items-center leading-normal no-underline text-white font-bold text-lg hover:text-primary text-[15px] border-0 border-b border-[#ffffff1a] border-solid'>{item}</Link>
                                             )
                                         }
                                     })}
--- src/components/Main/home_components/TopPackages.jsx
+++ src/components/Main/home_components/TopPackages.jsx
@@ -1,8 +1,88 @@
 import React from 'react'
+import { HiLocationMarker } from 'react-icons/hi'
+import mountain from '../../../assets/mountain.jpg'
 
 function TopPackages() {
+  const Card = ({ image, text, location }) => {
+    return (
+      <div >
+        <div className='overflow-hidden relative my-0 mx-auto rounded-2xl'>
+          <img src={image} alt='' className='rounded-2xl w-[300px] h-[300px] box-border hoverImg' />
+          <p className=' bg-white rounded-lg p-1 text-sm absolute top-4 right-4'>20% off</p>
+        </div>
+        <div>
+          <h5 className='text-xl pt-2 font-semibold'>{text}</h5>
+          <div className='flex items-center gap-2 py-3'>
+            <HiLocationMarker size={20} className='text-primary' />
+            <p className='text-gray text-light'>{location}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
   return (
-    <div>TopPackages</div>
+    <div className='container mx-auto px-3'>
+      <section className='pt-16'>
+        <div>
+          <div className='lg:flex justify-between text-center'>
+            <span>
+              <h2 className='text-4xl'>Top Packages</h2>
+              <p className='py-4'>Let&apos;s enjoy this heaven on earth</p>
+            </span>
+            <p className="text-light lg:w-2/5">
+              Many places are very famous, beautiful, clean, and will give very deep impression to visitors and make them come back.
+            </p>
+          </div>
+          <div className="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6 my-12 relative z-10">
+            <Card image={mountain} location='Mils, Tirol, Austria' text='Mils, Tirol' />
+            <Card image={mountain} location='Mils, Tirol, Austria' text='Mils, Tirol' />
+            <Card image={mountain} location='Mils, Tirol, Austria' text='Mils, Tirol' />
+            <Card image={mountain} location='Mils, Tirol, Austria' text='Mils, Tirol' />
+          </div>
+        </div>
+      </section>
+      <section className="py-16">
+        <span className="flex flex-col items-center text-center gap-2">
+          <h2 className="text-4xl font-bold">Travel to make sweet memories.</h2>
+          <p>Find trips that fit a flexible lifestyle</p>
+        </span>
+        <div className="lg:flex items-center justify-between py-16">
+          <div className="1g:w-1/2">
+
+            <div >
+              <span className="bg-[#7bbbde] text-white px-3 py-1 rounded-xl">01</span>
+              <h2 className="font-bold text-lg py-4">Find trips that fit your freedom</h2>
+              <p className="text-light pb-6 lg:w-1/2">
+                Traveling offers freedom and flexibility, solitude and
+                spontaneitv. and privacy and purpose
+              </p>
+            </div>
+
+            <div>
+              <span className="bg-[#8196e9] text-white px-3 py-1 rounded-xl">02</span>
+              <h2 className="font-bold text-lg py-4">Get back to nature by travel</h2>
+              <p className="text-light pb-6 lg:w-1/2">
+                The world is a playground and you can finally explore Mother
+                Nature&apos;s inimitable canvas.
+              </p>
+            </div>
+
+            <div>
+              <span className="bg-primary text-white px-3 py-1 rounded-xl">03</span>
+              <h2 className="font-bold text-lg py-4">Reignite those travel tastebuds</h2>
+              <p className="text-light pb-6 lg:w-1/2">
+                There are infinite reasons to love travel, one of them being the food, glorious food.
+              </p>
+            </div>
+            <button className="bg-primary text-white px-6 py-3 transition-bg hover:bg-white hover:text-primary rounded-xl shadow my-8">Start your Explore</button>
+          </div>
+          <div className="lg:w-1/2 h-full overflow-hidden rounded-2xl">
+            <img src={mountain} alt="" className="rounded-2xl w-full h-full hoverImg"/>
+          </div>
+        </div>
+
+      </section>
+    </div>
   )
 }
 
